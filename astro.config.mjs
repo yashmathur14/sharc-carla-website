ebe420b779686b5c630ae7bba63162e7d72d93cf
Yash Mathur <yashmathur@wifi-10-43-215-122.wifi.berkeley.edu>
2026-04-07 00:09:22 -0700

    Configure GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://yashmathur14.github.io',
+  base: '/sharc-carla-website',
   vite: {
     plugins: [tailwindcss()]
   }
